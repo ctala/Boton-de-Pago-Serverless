990d9748987c8cac059b548151e8c1a024ff7aaf
Cristian Tala <yomismo@cristiantala.cl>
2018-10-30 12:08:36 -0300

    Agregando Estructura

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Botón de Pago Serverless',
+    body: undefined
+  });
+});
+
+/* POST home page. */
+router.post('/', function (req, res, next) {
+  console.log(req.body);
+  res.render('index', {
+    title: 'Botón de Pago Serverless',
+    body: req.body
+  });
+});
+
+module.exports = router;
